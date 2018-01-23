0ef7f11bc6b19f904536d349a3483ae95313d10d
Ariable <wwwww818@gmail.com>
2018-01-24 03:12:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+利用 AQICN.ORG 的 WIDGET 做的一个小工具
+根据访客的ip地址显示其城市的 AQI
+用法就是将几个 PHP 文件都放到同一目录下
+前端只需要`<script src="yoursite.com/aqijs.php?size=xxx"></script>`
+即可使用
+size有small large xsmall xlarge xxl 这几种 按自己需求使用即可
+当然你的服务器需要支持 PHP
---
+++ aqijs.php
@@ -0,0 +1,107 @@
+<?php
+ include './pinyin.php';
+//获取当前ip
+function getIp(){
+  $onlineip='';
+  if(getenv('HTTP_CLIENT_IP')&&strcasecmp(getenv('HTTP_CLIENT_IP'),'unknown')){
+   $onlineip=getenv('HTTP_CLIENT_IP');
+  } elseif(getenv('HTTP_X_FORWARDED_FOR')&&strcasecmp(getenv('HTTP_X_FORWARDED_FOR'),'unknown')){
+   $onlineip=getenv('HTTP_X_FORWARDED_FOR');
+  } elseif(getenv('REMOTE_ADDR')&&strcasecmp(getenv('REMOTE_ADDR'),'unknown')){
+   $onlineip=getenv('REMOTE_ADDR');
+  } elseif(isset($_SERVER['REMOTE_ADDR'])&&$_SERVER['REMOTE_ADDR']&&strcasecmp($_SERVER['REMOTE_ADDR'],'unknown')){
+   $onlineip=$_SERVER['REMOTE_ADDR'];
+  }
+  return $onlineip;
+ }
+
+ //获取城市信息api
+ function getLocation($ip){
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=".$ip);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+  $str = curl_exec($curl);
+  curl_close($curl);
+  return $str;
+ }
+
+ //当前ip地址
+ $currentIP = getIp();
+
+ //通过当前ip获取信息
+ $getLocation = getLocation($currentIP);
+ $currentInfo = json_decode($getLocation, true); 
+
+ //判断ip是否为有效
+ if($currentInfo['ret'] == '-1')
+ {
+  $currentInfo['city'] = 'unknown';
+ }
+
+ //当前城市中文名
+ $currentCityName = $currentInfo['city'];  
+ $currentCityEName = $pin->Pinyin("$currentCityName",'UTF8');
+
+ //城市拼音多音字
+ switch($currentCityEName)
+ {
+  case 'zhongqing':
+   $currentCityEName = 'chongqing';
+  break;
+
+  case 'shenfang':
+   $currentCityEName = 'shifang';
+  break;
+
+  case 'chengdou':
+   $currentCityEName = 'chengdu';
+  break;
+
+  case 'yueshan':
+   $currentCityEName = 'leshan';
+  break;
+
+  case 'junxian':
+   $currentCityEName = 'xunxian'; 
+  break;
+
+  case 'shamen':
+   $currentCityEName = 'xiamen'; 
+  break;
+
+  case 'zhangsha':
+   $currentCityEName = 'changsha'; 
+  break;
+
+  case 'weili':
+   $currentCityEName = 'yuli'; 
+  break;
+
+  case 'zhaoyang':
+   $currentCityEName = 'chaoyang'; 
+  break;
+
+  case 'danxian':
+   $currentCityEName = 'shanxian'; 
+  break;
+
+  default:
+   $currentCityEName = $pin->Pinyin("$currentCityName",'UTF8');
+  break;
+ }
+$size=$_GET['size'];
+function getjscode($size,$location){
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, "https://widgets.waqi.info/jswgt/?size=$size&city=$location&lang=cn");
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+  $str = curl_exec($curl);
+  curl_close($curl);
+  return $str;
+ }
+header('Content-type: text/javascript');
+$jscode=getjscode($size,$currentCityEName);
+echo $jscode;
+?>
+
